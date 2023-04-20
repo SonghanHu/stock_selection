e34fad1c7c80785bc5c01db9b6db06168b5a0841
Hanson <128550319+SonghanHu@users.noreply.github.com>
2023-04-20 15:09:57 -0700

    Update stock_strategy1.py

--- stock_strategy1.py
+++ stock_strategy1.py
@@ -3,12 +3,11 @@ import yfinance as yf
 
 class stock_selection:
 
-    def __init__(self, day_range) -> None:
+    def __init__(self, day_range):
 
         sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
         self.ticker = list(sp500[0]['Symbol'])
         self.day = day_range
-        self.long = [], self.short = []
 
     def simple_strategy(self):
 
@@ -17,16 +16,16 @@ class stock_selection:
 
         try:
             for i in range(len(self.ticker)):
-                da = yf.Ticker(self.ticker[i])
-                da1 = [self.ticker[i]] = da.history(period = '1mo').iloc[-self.day:,[0,3]]
-                data[self.ticker[i]] = (da1.iloc[0,0],da1.iloc[-1,1])
-                rate = (data[self.ticker[i]][1] - data[self.ticker[i]][0]) / data[self.ticker[i]][0]
-                semi[self.ticker[i]] = rate
+                yfin = yf.Ticker(self.ticker[i])
+                da = yfin.history(period = '1mo').iloc[-self.day:,[0,3]]
+                data[self.ticker[i]] = (da.iloc[0,0],da.iloc[-1,1])
+                if (data[self.ticker[i]][0] > 2) & (data[self.ticker[i]][1] > 2):
+                    rate = (data[self.ticker[i]][1] - data[self.ticker[i]][0]) / data[self.ticker[i]][0]
+                    semi[self.ticker[i]] = rate
                 
         except Exception:
             pass
-
+        
         indicator = sorted(semi.items(), key = lambda item: -item[1])
         self.short = [i[0] for i in indicator[:20]]
         self.long = [i[0] for i in indicator[-20:]]
-        
